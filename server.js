597837e16fb1a70972c2427f9163a7264a145225
57607_Bhagyashri <209bbhagyashri@gmail.com>
2022-12-29 16:22:20 +0530

    done

---
+++ server.js
@@ -0,0 +1,31 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+
+const app=express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/index.html");
+});
+app.get("/about", function(req,res){
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.get("/home", function(req,res){
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/",function(req,res){
+    var num1=Number(req.body.num1);
+    console.log(num1);
+    var num2=Number(req.body.num2);
+    console.log(num2);
+    var result=num1+num2;
+
+
+    res.send("The result of calculation is "+result);
+});
+
+app.listen(1000, function(){
+    console.log("Server is running on port 1000");
+});
